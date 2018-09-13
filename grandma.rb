007ea09918294ab01f5827c3cbf66cb48ce364f4
Victor Gonzalez <mywalletislost@gmail.com>
2018-09-13 21:29:15 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
 
+
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
