fb701483c157ac949679ef31d536118036ccfb7f
Victor Gonzalez <mywalletislost@gmail.com>
2018-09-13 21:36:36 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,12 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
 
-if phrase.upcase == "I LOVE YOU GRANDMA!"
+  if phrase.upcase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-elsif phrase.upcase == phrase
-  "NO, NOT SINCE 1938!"
-else 
-    "HUH?! SPEAK UP, SONNY!"
+  elsif phrase.upcase == phrase
+    "NO, NOT SINCE 1938!"
+  else 
+     "HUH?! SPEAK UP, SONNY!"
   end
 end
 
