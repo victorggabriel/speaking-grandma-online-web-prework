6944e6612671f62e0b62a0a08dc0710030a9bcbd
Victor Gonzalez <mywalletislost@gmail.com>
2018-09-13 21:28:15 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,11 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma(phrase)
+if(phrase.upcase = "I LOVE YOU GRANDMA")
+  puts "I LOVE YOU TOO PUMPKIN"
+else 
+  puts
+end
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
