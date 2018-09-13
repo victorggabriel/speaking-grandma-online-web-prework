5558c0c18139c5c4312569c570c0dd5cc84c737e
Victor Gonzalez <mywalletislost@gmail.com>
2018-09-13 21:33:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
 
-if phrase.upcase = "I LOVE YOU GRANDMA"
-  puts "I LOVE YOU TO PUMPKIN!"
+if phrase.upcase = "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TO PUMPKIN!"
   
 end
 end
