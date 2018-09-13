db861963ab8c390a6cc68479ab49eb3c0be8e103
Victor Gonzalez <mywalletislost@gmail.com>
2018-09-13 21:36:11 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
 
 if phrase.upcase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOo PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
 elsif phrase.upcase == phrase
   "NO, NOT SINCE 1938!"
 else 
